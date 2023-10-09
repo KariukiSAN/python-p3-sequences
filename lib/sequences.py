efd7e4d945e3f630663baac9938e9cd9a03ab575
KariukiSAN <kariukian1@gmail.com>
2023-10-09 14:27:08 +0300

    Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 
+def print_fibonacci(length):
+    fib_sequence = []
+    if length > 0:
+        a, b = 0, 1
+        fib_sequence.append(a)
+        for _ in range(1, length):
+            a, b = b, a + b
+            fib_sequence.append(a)
+
+    print(fib_sequence)
+
+if __name__ == "__main__":
+
+    print_fibonacci(10)
